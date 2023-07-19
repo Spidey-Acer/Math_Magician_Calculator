35ef74d7359cc215f337290f008294e44f714d01
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-19 16:44:35 +0300

    Modify the button child component to hear events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Calculator.scss";
 import Button from "./button";
-import "../logic/calculate";
+import calculate from "../logic/calculate";
 
 const Calculator = () => {
   //Keeping the track of the state of the calculator
@@ -20,27 +20,53 @@ const Calculator = () => {
   //Rendering the calculator
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">
+        {calculatorData.next || calculatorData.total || "0"}
+      </div>
       <div className="buttons">
-        <Button label="AC" />
-        <Button label="+/-" />
-        <Button label="%" />
-        <Button label="÷" className="orange" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="x" className="orange" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" className="orange" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" className="orange" />
-        <Button label="0" className="big-button" />
-        <Button label="." />
-        <Button label="=" className="orange" />
+        <Button label="AC" onClick={() => clickHandler("AC")} />
+        <Button label="+/-" onClick={() => clickHandler("+/-")} />
+        <Button label="%" onClick={() => clickHandler("%")} />
+        <Button
+          label="÷"
+          onClick={() => clickHandler("÷")}
+          className="orange"
+        />
+        <Button label="7" onClick={() => clickHandler("7")} />
+        <Button label="8" onClick={() => clickHandler("8")} />
+        <Button label="9" onClick={() => clickHandler("9")} />
+        <Button
+          label="x"
+          onClick={() => clickHandler("x")}
+          className="orange"
+        />
+        <Button label="4" onClick={() => clickHandler("4")} />
+        <Button label="5" onClick={() => clickHandler("5")} />
+        <Button label="6" onClick={() => clickHandler("6")} />
+        <Button
+          label="-"
+          onClick={() => clickHandler("-")}
+          className="orange"
+        />
+        <Button label="1" onClick={() => clickHandler("1")} />
+        <Button label="2" onClick={() => clickHandler("2")} />
+        <Button label="3" onClick={() => clickHandler("3")} />
+        <Button
+          label="+"
+          onClick={() => clickHandler("+")}
+          className="orange"
+        />
+        <Button
+          label="0"
+          onClick={() => clickHandler("0")}
+          className="big-button"
+        />
+        <Button label="." onClick={() => clickHandler(".")} />
+        <Button
+          label="="
+          onClick={() => clickHandler("=")}
+          className="orange"
+        />
       </div>
     </div>
   );
