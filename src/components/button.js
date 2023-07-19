c0333c9511c3aa75afcf2e49fd4f6940dd2b5f66
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-19 13:31:26 +0300

    Fix linter errors

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Button.scss';
 
 const Button = ({ label, className }) => (
   <button type="button" className={className}>
