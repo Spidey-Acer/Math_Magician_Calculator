748b12c857b97fd4f77920963659d49f7aa27c48
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-19 09:28:32 +0300

    Add Calculator.js and Calculator.sass

---
+++ src/components/calculator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Calculator.scss';
+
+const Calculator = () => (
+  <div className="calculator">
+    <div className="display">0</div>
+    <div className="buttons">
+      <button type="button">C</button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button">/</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button">*</button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">-</button>
+      <button type="button">0</button>
+      <button type="button">.</button>
+      <button type="button">=</button>
+      <button type="button">+</button>
+    </div>
+  </div>
+);
+
+export default Calculator;
