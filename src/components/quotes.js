d822ddb99e64a5ffab30325988ad042f917dd45c
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-20 08:18:50 +0300

    Fetch data from the API

---
+++ src/components/quotes.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const Quote = () => {
+  //State that holds the quotes and manages it's loading and error states
+  const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  //Fetching from the API
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(
+        "https://api.api-ninjas.com/v1/quotes?category=" + category
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong! Status: " + response.status);
+      }
+      const data = await response.json();
+      setQuote(data.quote);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+};
+
+export default Quote;
