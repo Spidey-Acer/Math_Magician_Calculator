e30c2bebfe8a5c7e40f498fcf5bf3df6e8ff3b8c
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-20 13:22:58 +0300

    Fix the issues that made the quotes not to show

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,33 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 const Quote = () => {
-  // State that holds the quotes and manages it's loading and error states
-  const [quote, setQuote] = useState('');
+  const [quote, setQuote] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
-  // Fetching from the API
-  //   const fetchQuote = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         'https://api.api-ninjas.com/v1/quotes?humor',
-  //       );
-  //       if (!response.ok) {
-  //         throw new Error(`Something went wrong! Status: ${response.status}`);
-  //       }
-  //       const data = await response.json();
-  //       setQuote(data.quote);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setError(error.message);
-  //       setLoading(false);
-  //     }
-  //   };
-
+  // Fetching data
   useEffect(() => {
     const fetchQuote = async () => {
       try {
-        const category = 'happiness';
+        const category = 'men';
         const apiKey = '0DY4H1KwPw4hMxbGwTy0QA==q4jslPWDGXAxSLbc';
         const response = await fetch(
           `https://api.api-ninjas.com/v1/quotes?category=${category}`,
@@ -39,8 +21,9 @@ const Quote = () => {
         if (!response.ok) {
           throw new Error(`Something went wrong! Status: ${response.status}`);
         }
+
         const data = await response.json();
-        setQuote(data.quote);
+        setQuote(data[0]);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -56,7 +39,19 @@ const Quote = () => {
       <h1>Quote of the day</h1>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      {!loading && !error && <p>{quote}</p>}
+      {!loading && !error && (
+        <div>
+          <p>{quote.quote}</p>
+          <p>
+            -
+            {quote.author}
+          </p>
+          <p>
+            Category the quote fall in:
+            {quote.category}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
