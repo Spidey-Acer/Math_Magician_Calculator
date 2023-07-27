56d3f54ef3454f60029798b7c2cf05cc73055bd3
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-27 07:00:28 +0300

    Add styling to the calculatorpage

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Calculator from '../components/calculator';
+import './calculatorPage.scss';
 
 function CalculatorPage() {
   return (
-    <div>
-      <h1>Calculator</h1>
-      <Calculator />
+    <div className="calcHome">
+      <h1 className="calcHome__title">Lets do some math!</h1>
+      <div className="calc-container">
+        <Calculator />
+      </div>
     </div>
   );
 }
